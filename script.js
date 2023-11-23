c3235d571a6cfdb296ca1cd72a17980e06507cc3
Gustavo Jordan Ferreira <gustavojordan@gmail.com>
2023-11-23 10:15:03 -0300

    criaçao do arquivo HTML

---
+++ script.js
@@ -0,0 +1,28 @@
+var a =2;
+var b =7;
+var c = a + b;
+
+alert(c); 
+
+valor1 = 8;
+valor2 = "8";
+
+/* valor2 é o mesmo valor mas não é do mesmo tipo o valor2 é uma string pelo fato de "" */
+// se você quiser saber se é do mesmo valor e do mesmo tipo basta colocar !==
+total = (valor1 === valor2); 
+alert(total);
+
+
+var idade, eleitor;
+idade = 6;
+eleitor = (idade < 18) ? "Não Pode Votar porque" : "Pode Votar porque";
+
+alert(' A resposta é: ' + eleitor + ' a idade dele é: ' + idade);
+
+var idade, namorar;
+idade = 14;
+namorar = (18 <= idade) ? " Pode namorar porque" : " Não pode namora porque";
+
+alert('A resposta é:' + namorar + ' a idade dele é de: ' + idade);
+
+
