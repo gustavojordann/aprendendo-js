ac287381d4d369e621b3350ef2c7901acc142fc0
Gustavo Jordan Ferreira <gustavojordan@gmail.com>
2023-11-29 08:59:48 -0300

    novo assunto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var a =2;
+/*var a =2;
 var b =7;
 var c = a + b;
 
@@ -7,7 +7,7 @@ alert(c);
 valor1 = 8;
 valor2 = "8";
 
-/* valor2 é o mesmo valor mas não é do mesmo tipo o valor2 é uma string pelo fato de "" */
+// valor2 é o mesmo valor mas não é do mesmo tipo o valor2 é uma string pelo fato de "" 
 // se você quiser saber se é do mesmo valor e do mesmo tipo basta colocar !==
 total = (valor1 === valor2); 
 alert(total);
@@ -23,6 +23,43 @@ var idade, namorar;
 idade = 14;
 namorar = (18 <= idade) ? " Pode namorar porque" : " Não pode namora porque";
 
-alert('A resposta é:' + namorar + ' a idade dele é de: ' + idade);
+alert('A resposta é:' + namorar + ' a idade dele é de: ' + idade);*/
 
 
+
+//Operadores lógicos 
+//&& é conhecido também como AND os dois tem que dar true
+
+// É como se fosse (E) os dois tem que dar true, então o nome tem que estar igual e a idade também.Para ele poder então entrar.
+
+/*var idade = 16;
+var nome = "João";
+
+if(nome == "João" && idade == 16) {
+    console.log("O João pode entrar na aula!")
+}else {
+    console.log("Não pode entrar obs: não é o João Ou Não tem 16 anos!")
+}*/
+
+
+
+//Operador lógico || 
+// É conhecido também como OR... ele retorna TRUE caso uma das operações retorne verdadeiro
+
+//O OR retorna false apenas se as duas expressões são falsas.
+
+// var idade = 16;
+// var nome = "João";
+
+ /*if(nome == "João" || idade > 14) {
+    console.log("O João pode entrar na aula!")
+ }else {
+    console.log("Não pode entrar!")
+}*/
+
+
+
+//obejct feito sozinho
+var myObject = {tipo: "moto", marca: "harley devidsom", ano: "2009"};
+
+alert(myObject.ano);
